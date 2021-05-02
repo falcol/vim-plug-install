14629e90cc045ba0b9b7923e2090a943f4eedba7
Đình Ngọc <43593736+falcol@users.noreply.github.com>
2021-05-02 09:56:11 +0700

    script install vim plug
    script install vim plug

---
+++ install-vimplug-scripts.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd
+
+sudo pacman -S neovim
+
+cd
+
+mkdir ~/.config/nvim
+
+cd nvim
+
+touch ~/.config/nvim/init.vim
+
+ls
+
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ls
+
+ls autoload
+
+mkdir ~/.config/nvim/vim-plug
+
+ls
+
+cd vim-plug
+
+touch ~/.config/nvim/vim-plug/plugins.vim
+
+cd
+
+cd ~/vim-plug-install
+
+cp plugins.vim ~/.config/nvim/vim-plug/plugins.vim
+cp init.vim ~/.config/nvim/init.vim
+
+echo "after all open -> https://www.chrisatmachine.com/Neovim/01-vim-plug/"
